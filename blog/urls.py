48604d4572e198796615b850dc76eb096586b1d7
Iskandar02 <aliff.iskandr02@gmail.com>
2024-10-15 21:45:52 +0900

    My Django Blog App, fix the Post app not appearing

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 ]
+
+admin.autodiscover()
